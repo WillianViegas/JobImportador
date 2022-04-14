5475e7621b8c859d004f845f4c5b8b0a0c6abd26
Willian Viegas <willianwed98@gmail.com>
2022-04-13 22:11:34 -0300

    Implementando métodos de download e scrapping

--- ImportFile.py
+++ ImportFile.py
@@ -1,14 +1,61 @@
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+import requests
 
-
-def download_arquivo():
+def download_urls_para_arquivo():
     try:
         #Obter urls para download de algum site
+        url = "https://www.imdb.com/calendar/?ref_=nv_mv_cal"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        lista_urls = []
+        linhas = 0
+
+        file = open("lista_urls.txt", "r")
+        conteudo = file.read()
+        co_lista = conteudo.split("\n")
+        for linha in co_lista:
+            if(linha):
+                linhas += 1
+
+        if linhas <= 0:
+            for a_href in soup.find_all("a", href=True):
+                if "imdb" not in a_href["href"] and "title" in a_href["href"]:
+                    lista_urls.append("https://www.imdb.com" + a_href["href"])
+
         #Salvar as urls em um arquivo txt
+        for url in lista_urls:
+            with open("lista_urls.txt", "a") as urlfile:
+                urlfile.write(url + "\n")
+
+        meu_arquivo = open("lista_urls.txt", "r")
+        linhas_arquivo = meu_arquivo.readlines()
+        urls_arquivo = linhas_arquivo
+
+        print(len(urls_arquivo))
+
+        for l in urls_arquivo:
+            print(l.strip())
+
         #Fazer tratativa no arquivo para nao repetir as urls
-        #realizar o download utilizando as urls salvas e guardar o que foi baixado em uma pasta
         print("download_arquivo")
+    except Exception as e:
+        print(e)
+
+def scrapping_dados_urls():
+    try:
+        print("scrapping_dados_urls")
+        meu_arquivo = open("lista_urls.txt", "r")
+        linhas_arquivo = meu_arquivo.readlines()
+        urls_arquivo = linhas_arquivo
+
+        for l in urls_arquivo:
+            url = l
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, "html.parser")
+            print(soup.find("h1"))
+        #realizar o download utilizando as urls salvas e guardar o que foi baixado em uma pasta
+
     except Exception as e:
         print(e)
 
@@ -24,5 +71,6 @@ def importar_dados():
         print(e)
 
 
-download_arquivo()
+# download_urls_para_arquivo()
+scrapping_dados_urls()
 importar_dados()
